31d85df01418bf060023c919bc1834366644e30e
wade <johnny12150@gmail.com>
2018-09-13 16:03:16 +0800

    Update navbar
    * 但是cdn的bootstrap不支援dropdown與collapse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // will appear at app-root (app.component.html as {{title}})
   title = 'app';
+  public isCollapsed = false;
 }
