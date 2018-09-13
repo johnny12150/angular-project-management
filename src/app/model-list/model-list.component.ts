2202afd72c3935df00f48f9a02dde331dc8e0744
wade <johnny12150@gmail.com>
2018-09-13 14:33:39 +0800

    Using template in typescript to replace html & css

---
+++ src/app/model-list/model-list.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-model-list',
+  template: `<h1>Template works</h1>`
+  // templateUrl: './model-list.component.html',
+  // styleUrls: ['./model-list.component.css']
+})
+export class ModelListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
