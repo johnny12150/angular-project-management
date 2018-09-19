11e7c23d1d3c371bf5de3179904219f273f07f72
wade <johnny12150@gmail.com>
2018-09-19 10:53:44 +0800

    router-outlet name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProjectListComponent} from './project-list/project-list.component';
 import {ModelListComponent} from './model-list/model-list.component';
+import {ProjectDetailComponent} from './project-detail/project-detail.component';
 
 const routes: Routes = [
   {path: 'projects', component: ProjectListComponent},
-  {path: 'models', component: ModelListComponent}
+  {path: 'models', component: ModelListComponent},
+  // 每頁都會有這個component
+  // {path: '', component: ProjectDetailComponent, outlet: 'detail' }
 ];
 
 @NgModule({
