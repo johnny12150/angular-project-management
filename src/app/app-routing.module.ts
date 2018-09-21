69b70e8885abd0ff0068cf93e3c0bff11ac999a1
wade <johnny12150@gmail.com>
2018-09-21 11:19:25 +0800

    Impement Login Form with materialize.js
    *Issue: 他和bootstrap會互相干擾

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProjectListComponent} from './project-list/project-list.component';
 import {ModelListComponent} from './model-list/model-list.component';
 import {ProjectDetailComponent} from './project-detail/project-detail.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
 
 const routes: Routes = [
   {path: 'projects', component: ProjectListComponent},
   {path: 'models', component: ModelListComponent},
+  {
+    path: 'Login', component: UserComponent,
+    children: [{path: '', component: LoginComponent}]
+  },
   // 每頁都會有這個component
   // {path: '', component: ProjectDetailComponent, outlet: 'detail' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ProjectListComponent} from './project-list/project-list.component';
@@ -11,13 +12,17 @@ import {MatButtonModule} from '@angular/material';
 import {MatIconModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ProjectDetailComponent} from './project-detail/project-detail.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectListComponent,
     ModelListComponent,
-    ProjectDetailComponent
+    ProjectDetailComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +30,8 @@ import {ProjectDetailComponent} from './project-detail/project-detail.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
