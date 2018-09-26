61aa05372e45b2c91bc930cb490c3172b2430236
wade <johnny12150@gmail.com>
2018-09-26 09:48:35 +0800

    Implement Login submit passing values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: string;
+  password: string;
 
-  constructor() {
+
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  login(UserName, password) {
+    console.log(UserName, password);
+    this.router.navigate(['/']);
   }
 
 }
