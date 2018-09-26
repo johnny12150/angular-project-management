959d2059287ed409c816804b477aad87ad571927
wade <johnny12150@gmail.com>
2018-09-26 10:49:57 +0800

    Login to API and get JWTtoken

--- src/app/fetch-project.service.ts
+++ src/app/fetch-project.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 // for angular6/ RxJS6
 // @ref: https://stackoverflow.com/questions/49840152/i-get-an-error-when-learning-angular-has-no-exported-member-observable
 import {Observable, of} from 'rxjs';
@@ -12,12 +12,19 @@ import {IProjects} from './projects';
 
 export class FetchProjectService {
   private API_URI = 'http://img-server.yolo.dev.annotation.taieol.tw';
+  readonly AuthUrl = 'http://apis.yolo.dev.annotation.taieol.tw/api';
 
   constructor(private fetch_project: HttpClient) {
   }
 
   getProjectList(): Observable<IProjects[]> {
     return this.fetch_project.get<IProjects[]>(this.API_URI);
+  }
+
+  tryLogin(user, password) {
+    const data = 'username=' + user + '&password=' + password;
+    const reqHeader = new HttpHeaders({'Content-Type': 'application/x-www-urlencoded'});
+    return this.fetch_project.post(this.AuthUrl + '/Login', data, {headers: reqHeader});
   }
 }
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {FetchProjectService} from '../fetch-project.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   password: string;
 
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private loginService: FetchProjectService) {
   }
 
   ngOnInit() {
@@ -19,6 +20,10 @@ export class LoginComponent implements OnInit {
 
   login(UserName, password) {
     console.log(UserName, password);
+    this.loginService.tryLogin(UserName, password).subscribe((res_data: any) => {
+      console.log(res_data);
+      // localStorage.setItem('userToken',res_data.JWTtoken);
+    });
     this.router.navigate(['/']);
   }
 
